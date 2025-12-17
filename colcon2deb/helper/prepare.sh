165783ba20a66180a0c2b57247808b395ffbc5c6
aeon <jerry73204@gmail.com>
2025-12-18 00:59:57 +0800

    Fix prepare.sh to avoid bash function export with /bin/sh

--- colcon2deb/helper/prepare.sh
+++ colcon2deb/helper/prepare.sh
@@ -24,15 +24,6 @@ make_pkg_work_dir() {
 }
 export -f make_pkg_work_dir
 
-clean_work_dir() {
-    set -e
-    work_dir=$1
-    shift
-    mkdir -p "$work_dir"
-    rm -f "$work_dir"/*.out "$work_dir"/*.err
-}
-export -f clean_work_dir
-
 # Only scan for packages if src directory exists
 # This will be populated by copy-src.sh later
 if [ -d "$colcon_work_dir/src" ]; then
@@ -40,17 +31,10 @@ if [ -d "$colcon_work_dir/src" ]; then
     # Directory creation is I/O bound, use fewer parallel jobs
     njobs_io=$(( ( $(nproc) + 1 ) / 2 ))
 
-    # Use unique semaphore ID for this operation
-    sem_id="prepare_$$_${RANDOM}"
-
-    colcon list --base-paths src | cut -f1-2 | \
-        while read -r pkg_name pkg_dir; do
-	    # Prepare the working directory for the package
-	    pkg_dir=$(realpath "$pkg_dir")
-	    pkg_work_dir="$(make_pkg_work_dir $pkg_name)"
-	    sem --id "$sem_id" "-j${njobs_io}" clean_work_dir "$pkg_work_dir"
-        done
-    sem --id "$sem_id" --wait
+    # Use GNU parallel instead of sem with exported functions
+    # This avoids bash function export issues with /bin/sh
+    colcon list --base-paths src | cut -f1 | \
+        parallel -j "$njobs_io" "mkdir -p '$pkg_build_dir'/{}; rm -f '$pkg_build_dir'/{}/*.out '$pkg_build_dir'/{}/*.err"
 else
     echo "info: skipping package directory cleanup (src not yet copied)"
 fi
